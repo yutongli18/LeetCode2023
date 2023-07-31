ca001b5b155591030e7a158b208436706cbb0be6
yutongli18 <22031212409@stu.xidian.edu.cn>
2023-07-31 09:12:51 +0800

    2023.7.31 344.反转字符串

---
+++ 2023.7.31-344.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        print(s)
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    sol.reverseString(s=["h", "e", "l", "l", "o"])
