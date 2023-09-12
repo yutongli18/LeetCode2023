16c1338a3acdbbe75c87d6467d6156d9d5e9fd7f
yutongli18 <22031212409@stu.xidian.edu.cn>
2023-09-12 09:00:45 +0800

    2023.9.12 111.二叉树的最小深度

---
+++ 2023.9.12.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        num_layers = 0
+        if root is None:
+            return num_layers
+        traverse_list = deque([root])
+        while len(traverse_list) > 0:
+            for _ in range(len(traverse_list)):
+                node = traverse_list.popleft()
+                if node.left is None and node.right is None:
+                    return num_layers + 1
+                if node.left is not None:
+                    traverse_list.append(node.left)
+                if node.right is not None:
+                    traverse_list.append(node.right)
+            num_layers += 1
