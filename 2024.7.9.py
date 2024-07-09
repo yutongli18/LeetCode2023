520df2cefc7192cc7da8e958f81096f0939cfe8e
yutongli18 <22031212409@stu.xidian.edu.cn>
2024-07-09 09:37:49 +0800

    2024年7月9日 236.二叉树的最近公共祖先

---
+++ 2024.7.9.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        236.二叉树的最近公共祖先
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+        left_node = self.lowestCommonAncestor(root.left, p, q)
+        right_node = self.lowestCommonAncestor(root.right, p, q)
+        if left_node == p and right_node == q or left_node == q and right_node == p:
+            return root
+        else:
+            return left_node if left_node else right_node
